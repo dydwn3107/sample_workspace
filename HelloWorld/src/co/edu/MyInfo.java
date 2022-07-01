4dafbfbc2d65b71e979059a0ca1e048d8f409526
admin <admin@YD-13>
2022-07-01 12:46:27 +0900

    first

---
+++ HelloWorld/src/co/edu/MyInfo.java
@@ -0,0 +1,22 @@
+package co.edu;
+
+public class MyInfo {
+	public static void main(String[] args) {
+		// 이름, 나이, 키
+		// 이름은 황용주이고, 나이는 28살이고, 172.3입니다.
+		
+		String myName = "황용주";
+		int myAge = 28;
+		double myHeight = 172.3;
+		
+		myName = "김덕용";
+	    myAge = 22;
+		myHeight = 173.1;
+		
+		System.out.println("이름은 " + myName + "이고, "
+							+ "나이는 " + myAge + "살이고, 키는 "
+											+ myHeight + "입니다.");
+	}
+			
+}
+	
