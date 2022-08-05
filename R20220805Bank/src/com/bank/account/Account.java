03822cdec3037392052aeff372735e48789df1e5
admin <admin@YD-13>
2022-08-05 17:45:18 +0900

    08052

---
+++ R20220805Bank/src/com/bank/account/Account.java
@@ -0,0 +1,40 @@
+package com.bank.account;
+
+import java.sql.Date;
+/*
+ * import java.util.Date;(년월일시분초)
+ * import java.sql.Date;(년월일)
+ */
+public class Account {
+	private String accountId;
+	private String memberId;
+	private int balance;
+	private Date credate;
+	
+	public String getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	public Date getCredate() {
+		return credate;
+	}
+	public void setCredate(Date credate) {
+		this.credate = credate;
+	} 
+	
+	
+}
